b61f5bceab3ceb36584f5653d227e07996f6fab8
Andrew Warner <wwarner.andrew@gmail.com>
2013-12-20 09:47:10 -0500

    Use PG by default, clean up Gemfile

--- simple_bootstrap_template.rb
+++ simple_bootstrap_template.rb
@@ -1,23 +1,3 @@
-gem "twitter-bootstrap-rails"
-gem 'decent_exposure'
-gem 'decent_generators'
-gem 'haml'
-gem 'haml-rails'
-gem 'pg'
-gem 'pry'
-
-gem_group :test, :development do
-  gem 'factory_girl'
-  gem 'fivemat'
-  gem 'pry-rails'
-  gem 'rspec-rails'
-  gem 'rspec'
-end
-
-gem_group :test do
-  gem 'shoulda-matchers'
-end
-
 ruby_version = `rvm current`.sub("\n",'')
 run "rvm gemset create #{app_name}"
 gemset = "#{ruby_version}@#{app_name}"
@@ -30,16 +10,63 @@ def new_file(name, content)
   create_file name, content, :verbose => false
 end
 
+run "rm Gemfile"
+new_file "Gemfile", <<-GEMFILE
+source 'https://rubygems.org'
+
+gem "decent_exposure"
+gem "decent_generators"
+gem "haml"
+gem "haml-rails"
+gem "pg"
+gem "pry"
+gem "pry-rails"
+gem "twitter-bootstrap-rails"
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'rails'
+gem 'sass-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :test, :development do
+  gem "factory_girl"
+  gem "fivemat"
+  gem "rspec-rails"
+  gem "rspec"
+end
+
+group :test do
+  gem "shoulda-matchers"
+end
+GEMFILE
+
 with_rvm 'gem install bundler'
 with_rvm 'bundle install'
 new_file '.ruby-gemset', app_name
 new_file '.ruby-version', ruby_version
 
+run "rm config/database.yml"
+new_file "config/database.yml", <<-DATABASE_YAML
+development:
+  adapter: postgresql
+  database: #{app_name}-dev
+  pool: 10
+  timeout: 5000
+
+test:
+  adapter: postgresql
+  database: #{app_name}-test
+  pool: 10
+  timeout: 5000
+DATABASE_YAML
+
 inside app_name do
   with_rvm 'rails generate bootstrap:install static'
   with_rvm 'rails generate controller static'
   with_rvm 'rails generate rspec:install'
-  with_rvm 'rake db:migrate'
+  with_rvm 'rake db:create db:migrate'
 end
 
 run 'rm app/views/layouts/application.html.erb'
