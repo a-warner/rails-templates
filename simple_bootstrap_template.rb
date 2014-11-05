d0f4cbd2619ba410395beff7d0a27bbdfe6f62dd
Andrew Warner <wwarner.andrew@gmail.com>
2014-11-05 00:19:25 -0500

    Can generate apps with user auth auto-configured

--- simple_bootstrap_template.rb
+++ simple_bootstrap_template.rb
@@ -1,10 +1,13 @@
 ruby_version = `rvm current`.sub("\n",'')
 run "rvm gemset create #{app_name}"
 gemset = "#{ruby_version}@#{app_name}"
+app_port = (rand * 10_000 + 1_000).to_i
 
 class_eval do
   define_method(:with_rvm) { |cmd| run "rvm #{gemset} do #{cmd}" }
 end
+
+configure_user_auth = ENV['USER_AUTH']
 
 def new_file(name, content)
   create_file name, content, :verbose => false
@@ -16,9 +19,9 @@ ruby '#{RbConfig::CONFIG['RUBY_PROGRAM_VERSION']}'
 source 'https://rubygems.org'
 
 gem "decent_exposure"
-gem "decent_generators"
+gem "decent_generators"#{%{\ngem "devise"} if configure_user_auth}
 gem "haml"
-gem "haml-rails"
+gem "haml-rails"#{%{\ngem "omniauth"} if configure_user_auth}
 gem "pg"
 gem "pry"
 gem "pry-rails"
@@ -68,10 +71,26 @@ inside app_name do
   with_rvm 'rails generate controller static'
   with_rvm 'rails generate rspec:install'
   with_rvm 'rake db:create db:migrate'
+  with_rvm 'gem install gem-ctags'
+  with_rvm 'gem ctags'
+
+  if configure_user_auth
+    with_rvm 'rails generate devise:install'
+    with_rvm 'rails generate devise user'
+    with_rvm 'rails generate devise:views'
+    with_rvm 'rake db:migrate'
+  end
 end
 
-run 'rm app/views/layouts/application.html.erb'
-new_file 'app/views/layouts/application.html.haml', File.read(File.expand_path('../application_layout.html.haml', __FILE__))
+if configure_user_auth
+  insert_into_file "config/environments/development.rb", "  config.action_mailer.default_url_options = { host: '#{app_name}.dev' }\n", :after => "Rails.application.configure do\n"
+end
+
+remove_file 'app/views/layouts/application.html.erb'
+new_file 'app/views/layouts/application.html.haml', ERB.new(File.read(File.expand_path('../application_layout.html.haml.erb', __FILE__))).result(binding)
+
+remove_file 'app/helpers/application_helper.rb'
+new_file 'app/helpers/application_helper.rb', File.read(File.expand_path('../application_helper.rb', __FILE__))
 
 run 'rm public/index.html'
 route "root to: 'static#index'"
@@ -84,6 +103,14 @@ new_file 'app/assets/stylesheets/global.sass', <<-GLOBAL_SASS
 body
   padding-top: 70px
 GLOBAL_SASS
+
+new_file 'bin/dev', <<-DEV_SERVER
+#!/usr/bin/env sh
+
+rails server -p #{app_port}
+DEV_SERVER
+
+chmod 'bin/dev', '+x'
 
 run "rm README.rdoc"
 new_file 'README.md', <<-README
